e249cf7e6429c1db195730ec4ef98c6858dab78e
henrylee2cn <henrylee2cn@gmail.com>
2020-01-24 17:51:24 +0800

    feat(address): add more
    Change-Id: I1a76fcdf9131b8faae2fa406a28284e4496790ad

--- address.go
+++ address.go
@@ -1,6 +1,76 @@
 package ameda
 
-// AddrInt returns a pointer int representing the address of i.
-func AddrInt(i int) *int {
+// StringAddr returns a string pointer representing the address of s.
+func StringAddr(s string) *string {
+	return &s
+}
+
+// BoolAddr returns a bool pointer representing the address of b.
+func BoolAddr(b bool) *bool {
+	return &b
+}
+
+// ByteAddr returns a byte pointer representing the address of b.
+func ByteAddr(b byte) *byte {
+	return &b
+}
+
+// IntAddr returns an int pointer representing the address of i.
+func IntAddr(i int) *int {
 	return &i
+}
+
+// Int8Addr returns an int8 pointer representing the address of i.
+func Int8Addr(i int8) *int8 {
+	return &i
+}
+
+// Int16Addr returns an int16 pointer representing the address of i.
+func Int16Addr(i int16) *int16 {
+	return &i
+}
+
+// Int32Addr returns an int32 pointer representing the address of i.
+func Int32Addr(i int32) *int32 {
+	return &i
+}
+
+// Int64Addr returns an int64 pointer representing the address of i.
+func Int64Addr(i int64) *int64 {
+	return &i
+}
+
+// UintAddr returns an uint pointer representing the address of u.
+func UintAddr(u uint) *uint {
+	return &u
+}
+
+// Uint8Addr returns an uint8 pointer representing the address of u.
+func Uint8Addr(u uint8) *uint8 {
+	return &u
+}
+
+// Uint16Addr returns an uint16 pointer representing the address of u.
+func Uint16Addr(u uint16) *uint16 {
+	return &u
+}
+
+// Uint32Addr returns an uint32 pointer representing the address of u.
+func Uint32Addr(u uint32) *uint32 {
+	return &u
+}
+
+// Uint64Addr returns an uint64 pointer representing the address of u.
+func Uint64Addr(u uint64) *uint64 {
+	return &u
+}
+
+// Float32Addr returns a float32 pointer representing the address of f.
+func Float32Addr(f float32) *float32 {
+	return &f
+}
+
+// Float64Addr returns a float64 pointer representing the address of f.
+func Float64Addr(f float64) *float64 {
+	return &f
 }
