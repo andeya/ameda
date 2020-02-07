76644941003b82c99500915696dba3ce191eefce
henrylee2cn <henrylee2cn@gmail.com>
2020-02-07 13:54:12 +0800

    feat(Uint8Slice): add Interfaces method
    Change-Id: I49ef6851b74563fbb52a6b0fbd9c70a110a5666b

--- slice_uint8.go
+++ slice_uint8.go
@@ -21,6 +21,15 @@ func (u Uint8Slice) Copy() []uint8 {
 	return b
 }
 
+// Interfaces converts uint8 slice to interface{} slice.
+func (u Uint8Slice) Interfaces() []interface{} {
+	r := make([]interface{}, len(u))
+	for k, v := range u {
+		r[k] = v
+	}
+	return r
+}
+
 // Strings converts uint8 slice to string slice.
 func (u Uint8Slice) Strings() []string {
 	r := make([]string, len(u))
