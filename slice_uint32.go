443afa751d9332290b8132d2e84154ad92177a4d
henrylee2cn <henrylee2cn@gmail.com>
2020-02-07 13:53:49 +0800

    feat(Uint32Slice): add Interfaces method
    Change-Id: Ic27ac2b0406a431ee756b97ea44264f28b6e1974

--- slice_uint32.go
+++ slice_uint32.go
@@ -22,6 +22,15 @@ func (u Uint32Slice) Copy() []uint32 {
 	return b
 }
 
+// Interfaces converts uint32 slice to interface{} slice.
+func (u Uint32Slice) Interfaces() []interface{} {
+	r := make([]interface{}, len(u))
+	for k, v := range u {
+		r[k] = v
+	}
+	return r
+}
+
 // Strings converts uint32 slice to string slice.
 func (u Uint32Slice) Strings() []string {
 	r := make([]string, len(u))
