2ac3a1f342393811d6ebd05690ae53b0c0418f53
henrylee2cn <henrylee2cn@gmail.com>
2020-02-08 20:51:01 +0800

    feat: update interface functions
    Change-Id: I580dbe76096767978b2b4f1891a004bd7c22c1f4

--- interface.go
+++ interface.go
@@ -30,6 +30,10 @@ func InterfaceToBool(i interface{}, emptyAsFalse ...bool) (bool, error) {
 		return v, nil
 	case nil:
 		return false, nil
+	case float32:
+		return Float32ToBool(v), nil
+	case float64:
+		return Float64ToBool(v), nil
 	case int:
 		return IntToBool(v), nil
 	case int8:
@@ -61,6 +65,8 @@ func InterfaceToBool(i interface{}, emptyAsFalse ...bool) (bool, error) {
 			return r.Bool(), nil
 		case reflect.Invalid:
 			return false, nil
+		case reflect.Float32, reflect.Float64:
+			return Float64ToBool(r.Float()), nil
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 			return Int64ToBool(r.Int()), nil
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
@@ -82,3 +88,56 @@ func InterfaceToBoolPtr(i interface{}, emptyAsFalse ...bool) (*bool, error) {
 	r, err := InterfaceToBool(i, emptyAsFalse...)
 	return &r, err
 }
+
+//
+// // InterfaceToFloat32 converts interface to float32.
+// func InterfaceToFloat32(i interface{},  emptyStringAsZero ...bool) (float32, error) {
+// 	switch v := i.(type) {
+// 	case bool:
+// 		return v, nil
+// 	case nil:
+// 		return false, nil
+// 	case int:
+// 		return IntToBool(v), nil
+// 	case int8:
+// 		return Int8ToBool(v), nil
+// 	case int16:
+// 		return Int16ToBool(v), nil
+// 	case int32:
+// 		return Int32ToBool(v), nil
+// 	case int64:
+// 		return Int64ToBool(v), nil
+// 	case uint:
+// 		return UintToBool(v), nil
+// 	case uint8:
+// 		return Uint8ToBool(v), nil
+// 	case uint16:
+// 		return Uint16ToBool(v), nil
+// 	case uint32:
+// 		return Uint32ToBool(v), nil
+// 	case uint64:
+// 		return Uint64ToBool(v), nil
+// 	case uintptr:
+// 		return v != 0, nil
+// 	case string:
+// 		return StringToBool(v, emptyAsFalse...)
+// 	default:
+// 		r := IndirectValue(reflect.ValueOf(i))
+// 		switch r.Kind() {
+// 		case reflect.Bool:
+// 			return r.Bool(), nil
+// 		case reflect.Invalid:
+// 			return false, nil
+// 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+// 			return Int64ToBool(r.Int()), nil
+// 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+// 			return Uint64ToBool(r.Uint()), nil
+// 		case reflect.String:
+// 			return StringToBool(r.String(), emptyAsFalse...)
+// 		}
+// 		if isEmptyAsZero(emptyAsFalse) {
+// 			return !r.IsZero(), nil
+// 		}
+// 		return false, fmt.Errorf("cannot convert %#v of type %T to bool", i, i)
+// 	}
+// }
